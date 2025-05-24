8360a959c88ed030b94c09beddfb27231b3bf975
RafaSantos19 <miasrafa@gmail.com>
2025-05-24 09:20:34 -0300

    first commit

---
+++ test/exemple.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Example Test Suite', function () {
+    it('deve somar dois números corretamente', function () {
+        assert.strictEqual(1 + 1, 2);
+    });
+});
